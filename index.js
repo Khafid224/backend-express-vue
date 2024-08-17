4e329254f3488f4fabdc50374d17f9f71af8f14c
Khafid224 <abdulhafid1230@gmail.com>
2024-08-17 20:02:37 +0700

    Membuat Restful API untuk Register

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const express = require('express')
 //import CORS
 const cors = require('cors')
 
+//import bodyParser
+const bodyParser = require('body-parser')
+
+//import router
+const router = require('./routes')
+
 //init app
 const app = express()
 
 //use cors
 app.use(cors())
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //define port
 const port = 3000;
@@ -17,6 +29,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//define routes
+app.use('/api', router);
 
 //start server
 app.listen(port, () => {
